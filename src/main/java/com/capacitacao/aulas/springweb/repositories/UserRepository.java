52597c023c0d33d06c67c7fd77c1507fe089fa70
Mario Henrique <mariomapedrao@gmail.com>
2023-11-28 14:47:59 -0300

    Injecao de dependencia - JPA Repository

---
+++ src/main/java/com/capacitacao/aulas/springweb/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.capacitacao.aulas.springweb.repositories;
+
+import com.capacitacao.aulas.springweb.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
