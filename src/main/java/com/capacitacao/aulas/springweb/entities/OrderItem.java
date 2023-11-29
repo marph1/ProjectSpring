e66160bf119df3e555f74055c654c3d388f74741
Mario Henrique <mariomapedrao@gmail.com>
2023-11-29 10:01:36 -0300

    Associacao ManyToMany #1

---
+++ src/main/java/com/capacitacao/aulas/springweb/entities/OrderItem.java
@@ -0,0 +1,83 @@
+package com.capacitacao.aulas.springweb.entities;
+
+import com.capacitacao.aulas.springweb.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "table_orderItem")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    private OrderItemPk id;
+    private Integer quantity;
+    private double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order setOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+    public Product getPrduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public OrderItemPk getId() {
+        return id;
+    }
+
+    public void setId(OrderItemPk id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
